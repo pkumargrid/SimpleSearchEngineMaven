e9038ab001fa2fab0f9018a694c79e7cb146d5cf
Pratik Kumar <prakumar@griddynamics.com>
2024-02-15 12:25:21 +0530

    Revert "made checkstyle errors 0"
    This reverts commit 1865b296573ef647431a001cc96609157ba84352.

--- src/main/java/Stage2.java
+++ src/main/java/Stage2.java
@@ -2,70 +2,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * A program that reads text lines from the standard input
- *          and processes single-word queries.
- *  The program must output all lines that contain the string
- *          from the query.
- *  For this stage, this should include the case
- *      where the query string appears as a substring of one of the text lines.
- *  For example, the query "bc" should be found in a line containing "abcd".
- */
-public final class Stage2 {
-
-    /**
-     * private constructor for Utility class.
-     */
-    private Stage2() {
-
-    }
-
-
-    /**
-     *
-     * @param toSearch word to search for
-     * @param listOfPerson array in which we have to look for the word
-     * @return returns matching documents in form of list<String>
-     *
-     */
-    private static List<String> getWordsThatMatch(
-            final String toSearch, final ArrayList<String> listOfPerson) {
-        List<String> matchingLines = new ArrayList<>();
-        for (String person : listOfPerson) {
-            if (person.contains(toSearch)) {
-                matchingLines.add(person);
+public class Stage2 {
+    private static List<String> getString(String word, ArrayList<String> list_of_persons) {
+        List<String> matching_lines = new ArrayList<>();
+        for (String s : list_of_persons) {
+            if (s.contains(word)) {
+                matching_lines.add(s);
             }
         }
-        return matchingLines;
+        return matching_lines;
     }
-
-    /**
-     * @param args to get command line arguments
-     */
-
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of people:");
         int noOfPerson = Integer.parseInt(scanner.nextLine());
-        ArrayList<String> listOfPerson = new ArrayList<>();
+        ArrayList<String> list_of_person = new ArrayList<>();
         System.out.println("Enter all people:");
-        while (noOfPerson-- > 0) {
-            listOfPerson.add(scanner.nextLine());
+        while(noOfPerson-- > 0){
+            list_of_person.add(scanner.nextLine());
         }
         System.out.println("Enter the number of search queries:");
         int queries = Integer.parseInt(scanner.nextLine());
-        while (queries-- > 0) {
+        while(queries-- > 0){
             System.out.println("Enter data to search people:");
-            String wordToSearch = scanner.nextLine();
-            List<String> find =
-                    getWordsThatMatch(wordToSearch.toLowerCase(), listOfPerson);
-            if (find.isEmpty()) {
+            String to_search = scanner.nextLine();
+            List<String> find = getString(to_search.toLowerCase(), list_of_person);
+            if(find.isEmpty()){
                 System.out.println("No matching people found.");
-            } else {
+            }
+            else{
                 System.out.println("Found people:");
                 find.forEach(System.out::println);
             }
         }
     }
 }
-
--- src/main/java/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Implementing the simplest search engine possible ever.
- * It should search for a specific word in a multi-word input line.
- */
